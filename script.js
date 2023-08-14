5a1ff8965c2b2584b9965f454e67ad9fd9f09bcd
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-08-14 21:45:50 +1000

    Add delete button, defined clearInputs so it will reset the input fields to empty values after an entry is successfully added

--- script.js
+++ script.js
@@ -6,37 +6,66 @@ const proteinInput = document.getElementById('protein');
 const carbsInput = document.getElementById('carbs');
 const addEntryButton = document.getElementById('addEntry');
 const entryList = document.getElementById('entryList');
+entryList.addEventListener('click', deleteEntry);
+const entries = [];
+
+function deleteEntry(event) {
+    if (event.target.classList.contains('delete-entry')) {
+      const entryIndex = parseInt(event.target.dataset.index, 10);
+      entries.splice(entryIndex, 1); // Remove the entry from the entries array
+      updateUI();
+    }
+  }
 
 addEntryButton.addEventListener('click', addEntry);
 
+
+
 function addEntry() {
-  const meal = mealInput.value;
-  const calories = parseInt(caloriesInput.value, 10);
-  const fiber = parseFloat(fiberInput.value);
-  const protein = parseFloat(proteinInput.value);
-  const carbs = parseFloat(carbsInput.value);
-
-  if (meal && !isNaN(calories) && !isNaN(fiber) && !isNaN(protein) && !isNaN(carbs)) {
-    const entry = { meal, calories, fiber, protein, carbs };
-    displayEntry(entry);
-    clearInputs();
-  } else {
-    console.log('Invalid input for entry.');
+    const meal = mealInput.value;
+    const calories = parseInt(caloriesInput.value, 10);
+    const fiber = parseFloat(fiberInput.value);
+    const protein = parseFloat(proteinInput.value);
+    const carbs = parseFloat(carbsInput.value);
+  
+    if (meal && !isNaN(calories) && !isNaN(fiber) && !isNaN(protein) && !isNaN(carbs)) {
+      const entry = { meal, calories, fiber, protein, carbs };
+      displayEntry(entry, entries.length); // Pass the index
+      entries.push(entry); // Add the new entry to the array
+      clearInputs(); // Call the clearInputs function
+    } else {
+      console.log('Invalid input for entry.');
+    }
   }
-}
-
-function displayEntry(entry) {
-  const listItem = document.createElement('li');
-  listItem.innerHTML = `
-    <strong>${entry.meal}</strong> - Calories: ${entry.calories}, Fiber: ${entry.fiber}g, Protein: ${entry.protein}g, Carbs: ${entry.carbs}g
-  `;
-  entryList.appendChild(listItem);
-}
-
-function clearInputs() {
-  mealInput.value = '';
-  caloriesInput.value = '';
-  fiberInput.value = '';
-  proteinInput.value = '';
-  carbsInput.value = '';
-}
+  
+  // Define the clearInputs function
+  function clearInputs() {
+    mealInput.value = '';
+    caloriesInput.value = '';
+    fiberInput.value = '';
+    proteinInput.value = '';
+    carbsInput.value = '';
+  }
+  
+  function displayEntry(entry, index) {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+      <strong>${entry.meal}</strong> - Calories: ${entry.calories}, Fiber: ${entry.fiber}g, Protein: ${entry.protein}g, Carbs: ${entry.carbs}g
+    `;
+  
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'delete-entry';
+    deleteButton.dataset.index = index; // Store the index of the entry
+    listItem.appendChild(deleteButton);
+  
+    entryList.appendChild(listItem);
+  }
+  
+  function updateUI() {
+    entryList.innerHTML = '';
+  
+    entries.forEach((entry, index) => {
+      displayEntry(entry, index);
+    });
+  }
