59adbd44e296f8048facb4e153dd8ffffc0dbc71
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-08-14 23:55:11 +1000

    Reverted back to a state where the code workedand more entries could be added.

--- script.js
+++ script.js
@@ -33,8 +33,7 @@ function addEntry() {
     const carbs = parseFloat(carbsInput.value);
     const rawDate = document.getElementById('date').value; // Get the selected date in yyyy-mm-dd format
     const dateParts = rawDate.split('-'); // Split the date into parts
-    const rawChartSize = document.getElementById('chartSize').value.trim().toLowerCase();
-    const canvasSize = rawChartSize === 'small' ? 150 : rawChartSize === 'large' ? 300 : 200;
+     
   
     if (meal && !isNaN(calories) && !isNaN(fiber) && !isNaN(protein) && !isNaN(carbs) && rawDate) {
       const formattedDate = `${dateParts[2]}/${dateParts[1]}/${dateParts[0].substring(2)}`;
@@ -44,8 +43,8 @@ function addEntry() {
         fiber,
         protein,
         carbs,
-        date: formattedDate,
-        chartSize: canvasSize
+        date: formattedDate
+        
       };
       entries.push(entry); // Add the new entry to the array
       clearInputs();
@@ -115,16 +114,6 @@ function addEntry() {
   function createPieChart(entry, index) {
     const chartElement = document.getElementById(`chart-${index}`);
     const ctx = chartElement.getContext('2d');
-
-    let canvasSize = 200; // Default size
-    if (entry.chartSize === 'small') {
-      canvasSize = 150;
-    } else if (entry.chartSize === 'large') {
-      canvasSize = 300;
-    }
-  
-    chartElement.width = canvasSize;
-    chartElement.height = canvasSize;
   
     const data = {
       labels: ['Calories', 'Fiber', 'Protein', 'Carbs'],
